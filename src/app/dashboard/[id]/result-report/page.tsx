cd1993e944df2aa0e2e2c8cf559bbe9662ce7449
hjkim <wh980213@naver.com>
2025-08-08 12:04:10 +0000

    feat: update result report page and styling

--- src/app/dashboard/[id]/result-report/page.tsx
+++ src/app/dashboard/[id]/result-report/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import ResultReportWidget from './components/ResultReportWidget';
+import './result-report.css';
 
 export default function ResultReport2Page() {
     return (
-        <div id="report-root" className="relative">
+        <div
+            id="report-root"
+            className="result-report-container"
+        >
             <ResultReportWidget />
         </div>
     );
